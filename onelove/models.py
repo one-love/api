ee14d518a38db886608328ff1b7b9d94d8da275c
Milorad Kubatović <mlrd@tilda.center>
2016-06-07 19:23:41 +0200

    Provision log

--- onelove/models.py
+++ onelove/models.py
@@ -156,3 +156,10 @@ class Provision(Document):
     service = ReferenceField(Service)
     user = ReferenceField(User)
     applications = ListField(EmbeddedDocumentField(Provision))
+
+class Log(EmbeddedDocument):
+    status = StringField(max_length=512)
+    galaxy_role = StringField(max_length=1024)
+
+    def __repr__(self):
+        return '<Provision >'
